e62ab5eef4079c377bf7ab8573b0e129fe1c2c4b
catwangyi <www.1121995621@qq.com>
2022-03-12 14:10:28 +0800

    2022.3.12

--- WaveUnet/prediction.py
+++ WaveUnet/prediction.py
@@ -1,15 +1,21 @@
 import torch
-import torchaudio
-import os
-
+import torchaudio, os
+from WaveUnet.model import WaveUnet
 
 if __name__ == "__main__":
-    net = torch.load("best_model.pth")
+    IN_CHANNEL_LIST = [1, 24, 28, 72, 96, 120, 144, 168, 192, 216, 240, 264]
+    OUT_CHANNEL_LIST = [24, 28, 72, 96, 120, 144, 168, 192, 216, 240, 264, 288]
+    # unet = WaveUnet.model.WaveUnet(IN_CHANNEL_LIST, OUT_CHANNEL_LIST)
+    # torch.save(unet, "test.pth")
+    net = WaveUnet(IN_CHANNEL_LIST, OUT_CHANNEL_LIST)
+    net.load_state_dict(torch.load("best_model", map_location='cpu'))
+    net.eval()
     with torch.no_grad():
-        net.eval()
         for root, dirs, files in os.walk("audio\\noisy"):
             for file in files:
                 input_sig, sr = torchaudio.load(root+"\\"+file)
                 input_sig = torch.unsqueeze(input_sig, dim=0)
                 pred = net(input_sig)
+                a = input_sig.detach().numpy()
+                b = pred.detach().numpy()
                 torchaudio.save("audio\pred"+"\\pred_"+file, torch.squeeze(pred, dim=0), sr)
--- WaveUnet/train.py
+++ WaveUnet/train.py
@@ -8,6 +8,7 @@ IN_CHANNEL_LIST = [1, 24, 28, 72, 96, 120, 144, 168, 192, 216, 240, 264]
 OUT_CHANNEL_LIST = [24, 28, 72, 96, 120, 144, 168, 192, 216, 240, 264, 288]
 DATASET_PATH = "D:\\voicebank"
 EPOCH = 50
+DEVICE = "cuda" if torch.cuda.is_available() else "cpu"
 
 
 def collate_fn(batch):
@@ -22,25 +23,30 @@ def collate_fn(batch):
 
 if __name__ == "__main__":
     dataset = MyDataset(DATASET_PATH)
-    dataloader = DataLoader(dataset, batch_size=8, shuffle=True)
+    dataloader = DataLoader(dataset, batch_size=8, shuffle=True, num_workers=2)
     net = WaveUnet(IN_CHANNEL_LIST, OUT_CHANNEL_LIST)
     loss_func = nn.MSELoss()
     best_loss = float('inf')
+    net.to(DEVICE)
     optimizer = torch.optim.Adam(net.parameters(), lr=1e-4)
     for epoch in range(EPOCH):
+        net.train()
         last_save = 0
         curr_num = 0
         for noisy, label in dataloader:
+            noisy = noisy.to(DEVICE)
             pred = net(noisy)
+            label = label.to(DEVICE)
             loss = loss_func(pred, label)
 
             print("epoch:{},\tloss:{},\tlast_save:{}\tnum:{}/{}".format(epoch,
                                                                         loss.item(),
                                                                         last_save,
                                                                         curr_num,
-                                                                        len(dataset)))
+                                                                        len(dataloader)))
             if loss.item() < best_loss:
-                torch.save(net, "best_model.pth")
+                torch.save(net.state_dict(), "best_model")
+                # a = torch.load("best_model.pth")
                 best_loss = loss.item()
                 last_save = curr_num
                 print("save model")
