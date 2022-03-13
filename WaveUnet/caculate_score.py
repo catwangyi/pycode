e389ec0af911052e6fa4b07761a574ff70fcd0ba
catwangyi <www.1121995621@qq.com>
2022-03-13 17:07:01 +0800

    2022.3.13

--- WaveUnet/caculate_score.py
+++ WaveUnet/caculate_score.py
@@ -1,34 +1,40 @@
+import numpy as np
+
 from pystoi import stoi
+
 import librosa
 import os
 from pesq import pesq
 
 
 def caculate_stoi(clean_file_list, denoised_file_list):
+    score = 0
     for clean_path, enhanced_path in zip(clean_file_list, denoised_file_list):
-        clean, sr = librosa.load(clean_path, sr=None)
-        enhanced, _ = librosa.load(enhanced_path, sr=None)
-        score = stoi(clean, enhanced, sr)
-        print(score)
+        clean, sr = librosa.load(clean_path, sr=None, dtype='int16')
+        enhanced, _ = librosa.load(enhanced_path, sr=sr, dtype='int16')
+        score += stoi(clean, enhanced, sr)
+    print(score / len(clean_file_list))
 
 
-def caculate_pesq(clean_file_list, denoised_file_list, mode='wb'):
+def caculate_pesq(clean_file_list, denoised_file_list):
+    score = 0
     for clean_path, enhanced_path in zip(clean_file_list, denoised_file_list):
         clean, sr = librosa.load(clean_path, sr=16000)
-        enhanced, _ = librosa.load(enhanced_path, sr=None)
-        score = pesq(sr, clean, enhanced, mode=mode)
-        print(score)
+        enhanced, _ = librosa.load(enhanced_path, sr=sr)
+        score += pesq(sr, clean, enhanced)
+        # print(score)
+    print(score / len(clean_file_list))
 
 
 if __name__ == "__main__":
     clean_file_list = []
     denoised_file_list = []
-    for root, dirs, files in os.walk("audio\clean"):
+    for root, dirs, files in os.walk("D:\\voicebank\\test\clean_testset_wav"):
         for file in files:
             clean_file_list.append(os.path.join(root, file))
 
-    for root, dirs, files in os.walk("audio\pred"):
+    for root, dirs, files in os.walk("D:\\voicebank\pred"):
         for file in files:
             denoised_file_list.append(os.path.join(root, file))
 
-    caculate_pesq(clean_file_list, denoised_file_list, mode='nb')
+    caculate_pesq(clean_file_list, denoised_file_list)
--- WaveUnet/prediction.py
+++ WaveUnet/prediction.py
@@ -7,9 +7,10 @@ if __name__ == "__main__":
     net = torch.load("best_model.pth")
     with torch.no_grad():
         net.eval()
-        for root, dirs, files in os.walk("audio\\noisy"):
+        for root, dirs, files in os.walk("D:\\voicebank\\test\\noisy_testset_wav"):
             for file in files:
                 input_sig, sr = torchaudio.load(root+"\\"+file)
                 pred = net(torch.unsqueeze(input_sig, dim=0))
                 pred = torch.squeeze(pred, dim=0)
-                torchaudio.save("audio\pred"+"\\pred_"+file, pred, sr)
+                torchaudio.save("D:\\voicebank\pred"+"\\pred_"+file, pred, sr)
+                print("save:{}".format(file))
